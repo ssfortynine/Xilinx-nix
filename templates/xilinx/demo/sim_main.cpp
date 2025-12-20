c31a946b3c86aaa783fd9a8bda397da3c942611e
ssfortynine <ssfortynine@gmail.com>
2025-12-20 22:35:05 +0800

    update sim_main.cpp

--- templates/xilinx/demo/sim_main.cpp
+++ templates/xilinx/demo/sim_main.cpp
@@ -2,8 +2,8 @@
 #include <verilated.h>
 #include "Vdemo.h"
 
-// 只有在 Nix 传入了 --trace 时，VM_TRACE 宏才会被定义
-#ifdef VM_TRACE
+// 注意：这里改为 #if 而不是 #ifdef
+#if VM_TRACE
 #include "verilated_vcd_c.h"
 #endif
 
@@ -11,38 +11,41 @@ int main(int argc, char** argv) {
     Verilated::commandArgs(argc, argv);
     Vdemo* top = new Vdemo;
 
-#ifdef VM_TRACE
-    // 只要开启了 Trace 编译选项，就自动初始化波形
+    // 使用 #if VM_TRACE 检查宏的值是否为 1
+#if VM_TRACE
+    std::cout << "[SIM] Trace support detected. Waveform will be saved to waveform.vcd" << std::endl;
     Verilated::traceEverOn(true);
     VerilatedVcdC* tfp = new VerilatedVcdC;
     top->trace(tfp, 99);
     tfp->open("waveform.vcd");
-    std::cout << "[SIM] Trace support detected. Waveform will be saved to waveform.vcd" << std::endl;
 #endif
 
     top->clk = 0;
     top->reset = 1;
+    top->enable = 0;
 
     int main_time = 0;
     while (main_time < 500) {
         if (main_time > 20) top->reset = 0;
+        if (main_time > 30) top->enable = 1;
         if ((main_time % 5) == 0) top->clk = !top->clk;
 
         top->eval();
 
-#ifdef VM_TRACE
-        // 自动 dump 波形
-        tfp->dump(main_time);
+#if VM_TRACE
+        if (tfp) tfp->dump(main_time);
 #endif
         main_time++;
     }
 
     top->final();
 
-#ifdef VM_TRACE
-    tfp->close();
-    delete tfp;
-    std::cout << "[SIM] Simulation finished. Waveform file closed." << std::endl;
+#if VM_TRACE
+    if (tfp) {
+        tfp->close();
+        delete tfp;
+        std::cout << "[SIM] Simulation finished. Waveform file closed." << std::endl;
+    }
 #endif
 
     delete top;
