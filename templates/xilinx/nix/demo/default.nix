dfd4f255b08607a0be8b7cf2aac7692923d848b0
ssfortynine <ssfortynine@gmail.com>
2025-12-20 19:56:48 +0800

    Add rtl flow in default.nix

--- templates/xilinx/nix/demo/default.nix
+++ templates/xilinx/nix/demo/default.nix
@@ -1,16 +1,21 @@
 { lib, newScope }:
 lib.makeScope newScope (
   scope:
+  let
+    designTarget = "Demo";
+  in
   {
+    # RTL
+    rtl = scope.callPackage ./rtl.nix { target = designTarget; };
     # Verilator
-    verilated = scope.callPackage ./verilated.nix;
+    verilated = scope.callPackage ./verilated.nix {
+      rtl = scope.rtl; 
+    };
     verilated-trace = scope.verilated.override { enable-trace = true; };
 
     # VCS
     vcs = scope.callPackage ./vcs.nix {
-      sv2023 = false;
-      vpi = true;
-      timescale = 1000;
+      rtl = scope.rtl;
     };
     vcs-trace = scope.vcs.override { enable-trace = true; };
     
