5f540a6bf4f0d67c638e1ec083a2e6c2ef2a7264
ssfortynine <ssfortynine@gmail.com>
2025-12-20 22:39:45 +0800

    Reorganize the demo folder

--- templates/xilinx/nix/demo/verilated.nix
+++ templates/xilinx/nix/demo/verilated.nix
@@ -37,7 +37,7 @@ stdenv.mkDerivation {
       --timing \
       --threads ${toString thread-num} \
       -O1 \
-      --exe ./testbench/sim_main.cpp \
+      --exe h/sim_main.cpp \
       --cc -f ${rtl}/filelist.f \
       --top ${rtl.target} \
       --Mdir obj_dir \
